07374b211b3724f314fe1dd5e70a32ec307cf341
tyrion.liang <tyrion.liang@ringcentral.com>
2025-07-14 19:00:12 +0800

    misc: init project

---
+++ HearthstoneTool/HearthstoneTool/HearthstoneToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  HearthstoneToolApp.swift
+//  HearthstoneTool
+//
+//  Created by tyrion.liang on 2025/7/14.
+//
+
+import SwiftUI
+
+@main
+struct HearthstoneToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HearthstoneTool/HearthstoneToolTests/HearthstoneToolTests.swift
@@ -0,0 +1,17 @@
+//
+//  HearthstoneToolTests.swift
+//  HearthstoneToolTests
+//
+//  Created by tyrion.liang on 2025/7/14.
+//
+
+import Testing
+@testable import HearthstoneTool
+
+struct HearthstoneToolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
