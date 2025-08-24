083bbe6b15907c4672d9c12e4a5622f829b4ec1c
tyrion.liang <tyrion.liang@ringcentral.com>
2025-08-24 13:36:49 +0800

    feat: can display above Hearthstone APP

--- HearthstoneTool/HearthstoneTool/HearthstoneToolApp.swift
+++ HearthstoneTool/HearthstoneTool/HearthstoneToolApp.swift
@@ -6,12 +6,69 @@
 //
 
 import SwiftUI
+import AppKit
 
 @main
 struct HearthstoneToolApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView() // 这个不会显示，我们用自定义面板
         }
+    }
+}
+
+class HearthstonePanel: NSPanel {
+    static let kHearthstonePanelTitle = "HearthstoneToolPanel"
+    
+    init() {
+        super.init(contentRect: NSMakeRect(0, 0, 400, 350),
+                  styleMask: [.borderless, .nonactivatingPanel],
+                  backing: .buffered,
+                  defer: false)
+        
+        self.level = NSWindow.Level(rawValue: 1000)
+        self.backgroundColor = NSColor.clear
+        self.isOpaque = false
+        self.hasShadow = true
+        
+        // Allow the panel to be overlaid in a fullscreen space
+        var collectionBehavior = self.collectionBehavior
+        collectionBehavior.insert([.fullScreenAuxiliary, .canJoinAllSpaces])
+        self.collectionBehavior = collectionBehavior
+        
+        self.isMovableByWindowBackground = true
+        
+        // 设置 SwiftUI 内容
+        let contentView = NSHostingView(rootView: ContentView())
+        contentView.frame = self.contentView!.bounds
+        contentView.autoresizingMask = [.width, .height]
+        self.contentView = contentView
+        
+        // 居中显示
+        self.center()
+        
+        print("Panel 配置完成: level=\(self.level.rawValue), behavior=\(self.collectionBehavior)")
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var panel: HearthstonePanel?
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // 创建并显示面板
+        panel = HearthstonePanel()
+        panel?.makeKeyAndOrderFront(nil)
+        
+        // 确保应用不会因为没有窗口而退出
+        NSApp.setActivationPolicy(.accessory)
+    }
+    
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        if let panel = panel {
+            panel.makeKeyAndOrderFront(nil)
+        }
+        return true
     }
 }
